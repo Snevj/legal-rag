5e99ec406d503c9f772d66e2a6034bd91a3e19ad
Snevj <krishanagopal236606@gmail.com>
2026-08-06 15:15:44 +0530

    checking the branch

--- backend/app/embeddings/embedder.py
+++ backend/app/embeddings/embedder.py
@@ -1,4 +1,8 @@
 from functools import lru_cache
+import os
+
+os.environ.setdefault("USE_TF", "0")
+os.environ.setdefault("TRANSFORMERS_NO_TF", "1")
 
 from sentence_transformers import SentenceTransformer
 
